92bbfd56f3eccb5f503c9b0d1ed371189ed70c3d
최재민 <colajeamin@gmail.com>
2022-05-24 17:26:20 +0900

    rm 1.1.1.1

--- vpn/script.js
+++
@@ -1,9 +0,0 @@
-let i = 0;
-$(window).load(()=>{
-    setTimeout(() => {
-        setInterval(() => {
-            i += 0.0001;
-            $('html').css('transform', `rotate(${i % 360}deg)`);
-        }, 10);
-    }, 500);
-});
